1f5d3546b5b3c891732e0f323c9eb0e9117b7639
Home <Home@LAPTOP-2E6V6ACM>
2024-07-05 18:57:28 +0300

    Add project files.

---
+++ MyMauiApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+       
+    }
+
+}
---
+++ MyMauiApp/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MyMauiApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Cinzel-VariableFont_wght.ttf", "CinzelVar");
+                    fonts.AddFont("Pacifico-Regular.ttf", "Pacifico");
+                    fonts.AddFont("RubikDoodleShadow-Regular.ttf", "RubikDoodle");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
