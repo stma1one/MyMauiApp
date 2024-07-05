b010a8c9fca09abe0c6475ba87ff991f46c518e1
Home <Home@LAPTOP-2E6V6ACM>
2024-07-06 01:01:39 +0300

    update

--- MyMauiApp/MainPage.xaml.cs
+++ MyMauiApp/MainPage.xaml.cs
@@ -10,7 +10,41 @@
            
         }
 
-       
+        /// <summary>
+        /// אירוע הקלקה
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnHide_Clicked(object sender, EventArgs e)
+        { 
+            Button button = (Button)sender;//sender as Button
+            if (button == btnHide)
+            {
+                if (count % 2 == 0)
+                {
+                    lblHide.IsVisible = false;
+                    btnHide.Text = "בטל הסתרה";
+                }
+                else
+                {
+                    lblHide.IsVisible = true;
+                    btnHide.Text = "בטל הסתרה";
+                }
+                count++;
+            }
+            
+
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblOldPass.Text = e.OldTextValue;
+        }
     }
 
 }
